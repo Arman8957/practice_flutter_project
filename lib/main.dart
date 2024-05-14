5544d972213f12f83327b220b0d367471a3af909
Md Arman <novicethought0@gmail.com>
2024-05-14 12:05:04 +0000

    my updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:product.dart';
+import 'product.dart';
 import 'products.dart';
 import 'cart_provider.dart';
 import 'product_card.dart' // Assuming you have a product_card.dart file
